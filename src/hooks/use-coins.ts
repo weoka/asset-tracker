5dff09ca2f7aa6e6aadf130662a91552ef7a31f5
Weoka <weokap@gmail.com>
2025-01-21 08:13:19 -0500

    Add usecoins hook

---
+++ src/hooks/use-coins.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import { getAllCoins } from "../services/coinpaprika-service";
+import { Coin } from "../types/Coin";
+
+export const useCoins = () => {
+  const [coins, setCoins] = useState<Coin[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const data = await getAllCoins();
+        setCoins(data);
+      } catch (err) {
+        console.log(err);
+        setError("Failed to load coins");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCoins();
+  }, []);
+
+  return { coins, loading, error };
+};

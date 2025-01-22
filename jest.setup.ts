dff177a487b0215d6e181efe67488f215e65bddf
Weoka <weokap@gmail.com>
2025-01-21 19:07:39 -0500

    Remove primeng css unit test errors

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,3 +2,16 @@ import "@testing-library/jest-dom";
 import { TextEncoder } from "util";
 
 global.TextEncoder = TextEncoder;
+
+let consoleSpy: jest.SpyInstance;
+beforeAll(() => {
+  consoleSpy = jest
+    .spyOn(global.console, "error")
+    .mockImplementation((message) => {
+      if (!message?.message?.includes("Could not parse CSS stylesheet")) {
+        global.console.warn(message);
+      }
+    });
+});
+
+afterAll(() => consoleSpy.mockRestore());
